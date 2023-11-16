829189507341356b9beb5468fb99147d0bf95530
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2023-11-16 17:12:53 -0300

    feat: componentizando o Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,7 +12,7 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
-              <img className={styles.avatar} src="https://github.com/isagarci4.png" />
+              <Avatar src="https://github.com/isagarci4.png"/>
 
               <strong>Isadora Garcia</strong>
               <span>Web Developer</span>
