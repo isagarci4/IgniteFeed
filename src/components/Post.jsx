6057bb6bdabb6a2ceca0cf9c4e1fbc60b2d89fb1
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2023-11-28 17:40:11 -0300

    feat: validando formulário de comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,7 +33,12 @@ export function Post({ author, content, publishedAt }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório')
   }
 
   function deleteComment(commentToDelete) {
@@ -46,6 +51,8 @@ export function Post({ author, content, publishedAt }) {
     })
     setComments(commentsWithoutDeletedOne)
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0
 
   return(
     <article className={styles.post}>
@@ -81,10 +88,14 @@ export function Post({ author, content, publishedAt }) {
           placeholder="Escreva um comentário..."
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
